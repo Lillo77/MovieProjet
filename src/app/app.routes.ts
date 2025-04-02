d130b25662e32caaa5fc312d60bbf74b74b46ae4
TigerTigre88 <luposolitario1712@gmail.com>
2025-04-02 17:51:04 +0200

    feat: create navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {FavoritesPageComponent} from './favorites-page/favorites-page.component';
+import {DetailsPageComponent} from './details-page/details-page.component';
+import {ListWatchPageComponent} from './list-watch-page/list-watch-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },// Rotta per la homepage
+  { path: 'favorite', component: FavoritesPageComponent },
+  { path: 'details/:id', component: DetailsPageComponent },
+  { path: 'list-watch', component: ListWatchPageComponent }
+];
--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-navbar-component',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navbar-component.component.html',
   styleUrl: './navbar-component.component.css'
 })
