b83910bc886aabd41e2a9a89d525c01d3c6943a8
Salvo <scarfi@stevejobs.academy>
2025-04-04 19:32:07 +0200

    Aggiunto lista card

--- src/app/card-component/card-component.component.ts
+++ src/app/card-component/card-component.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // ✅ Importa questo!
 
 @Component({
   selector: 'app-card-component',
-  imports: [],
+  imports: [CommonModule], // ✅ Aggiungi qui
   templateUrl: './card-component.component.html',
-  styleUrl: './card-component.component.css'
+  styleUrl: './card-component.component.css',
 })
 export class CardComponentComponent {
-
+  @Input() movies: any[] = []; // riceve la lista di film dal componente genitore
 }
